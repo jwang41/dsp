bc8d40e57c68e385f100ed923f29a97117ee0e48
jihua wang <jwang41@gmail.com>
2024-12-16 16:11:39 -0600

    project struc

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+
+project_name = "ds"
+list_of_files = [
+    ".gthub/workflows/.gitkeep",
+    f"src/{project_name} / _init_.py",
+    f"src/{project_name}/ components/_init_.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/configureation.py",
+    f"src/{project_name}/pipeline/_init_.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/_init_.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "research/reserach.ipynb",
+    "templates/index.html",
+    "app.py"    
+    ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory {filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empth file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already existed")
+
+
+
+
+
